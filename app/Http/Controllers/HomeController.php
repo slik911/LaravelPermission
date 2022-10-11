c12b367731c25ddeb80afc179448ef1cac63d8c3
Coder <35855530+slik911@users.noreply.github.com>
2022-10-11 07:33:37 +0100

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // Role::create(['name'=>'writer']);
+        // Permission::create(['name'=>'edit post']);
+        // $role = Role::findById(1);
+        // $permission = Permission::findById(1);
+        // $role->givePermissionTo($permission);
+        // $permission->removeRole($role);
+        // auth()->user()->givePermissionTo('edit post');
+        // auth()->user()->assignRole('writer');
+        // return auth()->user()->();
+        return view('home');
+    }
+}
